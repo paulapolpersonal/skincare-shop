49a755f5d6adb02cced9246a78754abda64bbd01
paulaapol <paula.pol@wolfpack-digital.com>
2024-02-18 20:27:34 +0200

    add configurations and modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+
+  runtimeConfig: {
+    baseURL: "",
+    public: {
+      apiBase: "/api",
+    },
+  },
+
+  modules: ["@nuxtjs/tailwindcss", "nuxt-headlessui", "@nuxtjs/google-fonts"],
+
+  css: ["~/assets/scss/main.scss"],
+
+  googleFonts: {
+    families: {
+      Roboto: true,
+      Inter: [400, 700],
+      "Josefin+Sans": true,
+      Lato: [100, 300],
+      Raleway: {
+        wght: [100, 400],
+        ital: [100],
+      },
+      "Crimson Pro": {
+        wght: "200..900",
+        ital: "200..700",
+      },
+      "Kode Mono": true,
+    },
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template lang="pug">
+  div.flex.justify-center
+    p Hello world
+</template>
+
+<script setup></script>
+
+<style scoped lang="scss"></style>
