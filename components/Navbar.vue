ca0e1d1dbbba0bc4869129a54de080fbebfc1d62
paulaapol <paula.pol@wolfpack-digital.com>
2024-03-14 21:37:27 +0200

    code-review-improvements

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,37 +1,35 @@
 <template lang="pug">
-  div.w-full.h-24.bg-black.rounded-b-sm.flex.justify-between.items-center.px-10
-    NuxtLink(to="/")
-      img(src="/logo.jpeg").w-16.h-16.rounded-full
-    div.flex
-      NuxtLink(v-for="item in menuItems" :to="`${item.value}`").text-white.ml-10.cursor-pointer {{ item.text }}
-    div.w-50
-      Icon(name="teenyicons:search-circle-outline" color="white").w-8.h-8.mr-3.cursor-pointer
-      Icon(name="pepicons-pop:cart-circle" color="white").w-8.h-8.cursor-pointer
+.w-full.h-24.bg-black.rounded-b-sm.flex.justify-between.items-center.px-10
+  NuxtLink(to="/")
+    img.w-16.h-16.rounded-full(src="/logo.jpeg")
+  .flex
+    NuxtLink.text-white.ml-10.cursor-pointer(
+      v-for="item in menuItems",
+      :to="`${item.value}`"
+    ) {{ item.text }}
+  .w-50
+    Icon.w-8.h-8.mr-3.cursor-pointer(
+      name="teenyicons:search-circle-outline",
+      color="white"
+    )
+    Icon.w-8.h-8.cursor-pointer(name="pepicons-pop:cart-circle", color="white")
 </template>
 
-<script>
-import { reactive } from "vue";
-
-export default {
-  setup() {
-    const menuItems = reactive([
-      {
-        text: "Home",
-        value: "/",
-      },
-      {
-        text: "Products",
-        value: "/products",
-      },
-      {
-        text: "Contact",
-        value: "/contact",
-      },
-    ]);
-
-    return { menuItems };
+<script setup>
+const menuItems = [
+  {
+    text: "Home",
+    value: "/",
   },
-};
+  {
+    text: "Products",
+    value: "/products",
+  },
+  {
+    text: "Contact",
+    value: "/contact",
+  },
+];
 </script>
 
 <style></style>
