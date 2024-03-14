555bd1d6cd95e8e103314464bc30891c289dd4d9
paulaapol <paula.pol@wolfpack-digital.com>
2024-03-14 21:43:00 +0200

    code-review-imrpovements

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,72 +1,63 @@
 <template lang="pug">
-  div.w-full.min-h-64.bg-black.rounded-b-sm.justify-between.items-center.p-10.bottom-0.text-white.font-light
-    div.grid.grid-cols-6.gap-6
-      div.flex.flex-col.col-span-2
-        p.mb-4.font-semibold About Paula
-        p.break-words Thousands of people have benefited from our services. But let's be real - there's no magic bullet, just science. That's why we do what we do
-      div.flex.flex-col
-        p.mb-4.font-semibold Pages
-        NuxtLink(v-for="item in menuItems" :to="`${item.value}`").cursor-pointer {{ item.text }}
-      div.flex.flex-col
-        p.mb-4.font-semibold Products
-        p(v-for="product in products").cursor-pointer {{ product }}
-      div.flex.flex-col
-        p.mb-4.font-semibold Help
-        p(v-for="item in helpSection").cursor-pointer {{ item }}
-      div.flex.flex-col
-        p.mb-4.font-semibold Contact us
-        div(v-for="item in contactItems").cursor-pointer.flex.items-center
-          Icon(:name="item.icon" color="white").w-4.h-4.mr-3
-          p {{ item.text }}
-    hr.my-10
-    div.flex.justify-between.items-center
-      p Copyright, All rights reserved by Paula
-      img(src="/logo.jpeg").w-16.h-16.rounded-full
-
+.w-full.min-h-64.bg-black.rounded-b-sm.justify-between.items-center.p-10.bottom-0.text-white.font-light
+  .grid.grid-cols-6.gap-6
+    .flex.flex-col.col-span-2
+      p.mb-4.font-semibold About Paula
+      p.break-words Thousands of people have benefited from our services. But let's be real - there's no magic bullet, just science. That's why we do what we do
+    .flex.flex-col
+      p.mb-4.font-semibold Pages
+      NuxtLink.cursor-pointer(v-for="item in menuItems", :to="`${item.value}`") {{ item.text }}
+    .flex.flex-col
+      p.mb-4.font-semibold Products
+      p.cursor-pointer(v-for="product in products") {{ product }}
+    .flex.flex-col
+      p.mb-4.font-semibold Help
+      p.cursor-pointer(v-for="item in helpSection") {{ item }}
+    .flex.flex-col
+      p.mb-4.font-semibold Contact us
+      div.cursor-pointer.flex.items-center(v-for="item in contactItems")
+        Icon.w-4.h-4.mr-3(:name="item.icon", color="white")
+        p {{ item.text }}
+  hr.my-10
+  .flex.justify-between.items-center
+    p Copyright, All rights reserved by Paula
+    img.w-16.h-16.rounded-full(src="/logo.jpeg")
 </template>
 
-<script>
-import { reactive } from "vue";
-
-export default {
-  setup() {
-    const menuItems = reactive([
-      {
-        text: "Home",
-        value: "/",
-      },
-      {
-        text: "Products",
-        value: "/products",
-      },
-      {
-        text: "Contact",
-        value: "/contact",
-      },
-    ]);
-
-    const products = reactive(["Cleansers", "Serums", "Creams", "Toners"]);
-
-    const helpSection = reactive(["Shipping and return", "Track order", "FAQ"]);
-
-    const contactItems = reactive([
-      {
-        text: "hello@paula.com",
-        icon: "ri:send-plane-fill",
-      },
-      {
-        text: "(021) 555-0124",
-        icon: "material-symbols:call",
-      },
-      {
-        text: "2618 Ocean City, Maryland",
-        icon: "material-symbols:location-on-rounded",
-      },
-    ]);
-
-    return { menuItems, products, helpSection, contactItems };
+<script setup>
+const menuItems = [
+  {
+    text: "Home",
+    value: "/",
   },
-};
+  {
+    text: "Products",
+    value: "/products",
+  },
+  {
+    text: "Contact",
+    value: "/contact",
+  },
+];
+
+const products = ["Cleansers", "Serums", "Creams", "Toners"];
+
+const helpSection = ["Shipping and return", "Track order", "FAQ"];
+
+const contactItems = [
+  {
+    text: "hello@paula.com",
+    icon: "ri:send-plane-fill",
+  },
+  {
+    text: "(021) 555-0124",
+    icon: "material-symbols:call",
+  },
+  {
+    text: "2618 Ocean City, Maryland",
+    icon: "material-symbols:location-on-rounded",
+  },
+];
 </script>
 
 <style></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -4,13 +4,7 @@
   <Footer />
 </template>
 
-<script>
-import Navbar from "~/components/Navbar.vue";
-import Footer from "~/components/Footer.vue";
-
-export default {
-  components: { Navbar, Footer },
-};
+<script setup>
 </script>
 
 <style></style>
